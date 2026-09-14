449fbfb61222f54dc90982648af0e3030d9bcace
JAGATHEESAN <jagatheesancud@gmail.com>
2026-09-14 09:03:42 +0530

    Delete ex11 directory

--- ex11/setup.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE studentdb;
-USE studentdb;
-
-CREATE TABLE students (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    age INT NOT NULL,
-    course VARCHAR(100) NOT NULL
-);
